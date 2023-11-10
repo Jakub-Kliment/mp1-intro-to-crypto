cff095e4c9afbdd618343607d40b86afe4cb1399
Jakub Kliment <jakub.kliment@epfl.ch>
2023-11-10 15:50:55 +0100

    assert pour image

--- src/main/java/ch/epfl/cs107/stegano/ImageSteganography.java
+++ src/main/java/ch/epfl/cs107/stegano/ImageSteganography.java
@@ -69,7 +69,7 @@ public final class ImageSteganography {
         assert nullRow(cover) && nullRow(load);
         assert shapeCompatibility(cover) && shapeCompatibility(load);
         assert emptyImage(cover) && emptyImage(load);
-        //assert sizeCompatibility(cover, load);
+        assert cover[0].length == load[0].length;
 
         int[][] newImage = new int[cover.length][cover[0].length];
         int index = 0;
